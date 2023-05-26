22f46110690f8e016cfb96c46fa10c99bcf819d3
mehdi <midomoubarak24@gmail.com>
2023-05-26 22:55:06 +0100

    renamed-index-files

--- client/src/pages/Layout.js
+++ client/src/pages/Layout.js
@@ -2,17 +2,17 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
-import Contact from "./Contact/Index";
-import About from "./About/Index";
+import Contact from "./Contact/index";
+import About from "./About/index";
 import Shop from "./Shop/index";
-import Services from "./Services/Index";
-import Cart from "./Cart/Index";
+import Services from "./Services/index";
+import Cart from "./Cart/index";
 import Home from "./Home/index";
 import Login from "./Auth/Login";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import Signup from "./Auth/Signup";
-import Checkout from "./Checkout/Index";
+import Checkout from "./Checkout/index";
 function Layout() {
   return (
     <div>
